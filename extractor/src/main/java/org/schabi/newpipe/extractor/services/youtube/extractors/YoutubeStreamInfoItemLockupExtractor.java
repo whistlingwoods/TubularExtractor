3c5a286cf0bca8860e0e2166c961d56d4a083a5d
G-flat <63449095+G-flat@users.noreply.github.com>
2026-03-01 16:37:43 +1100

    Fix fetching YouTube item duration

--- extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeStreamInfoItemLockupExtractor.java
+++ extractor/src/main/java/org/schabi/newpipe/extractor/services/youtube/extractors/YoutubeStreamInfoItemLockupExtractor.java
@@ -156,8 +156,8 @@ public class YoutubeStreamInfoItemLockupExtractor implements StreamInfoItemExtra
                 "contentImage.thumbnailViewModel.overlays")
             .streamAsJsonObjects()
             .flatMap(jsonObject -> jsonObject
-                .getObject("thumbnailOverlayBadgeViewModel")
-                .getArray("thumbnailBadges")
+                .getObject("thumbnailBottomOverlayViewModel")
+                .getArray("badges")
                 .streamAsJsonObjects())
             .map(jsonObject -> jsonObject
                 .getObject("thumbnailBadgeViewModel")
